f4eb8c5e500c1a968860f1c0f6b26d99e79a1781
Luiz Passos <luizhmpassos@gmail.com>
2018-07-11 00:03:28 +0000

    Envio Inicial

---
+++ src/main/java/com/javaee/luizhmpassos/springtests/repositories/VendorRepository.java
@@ -0,0 +1,11 @@
+package com.javaee.luizhmpassos.springtests.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javaee.luizhmpassos.springtests.domain.Vendor;
+
+@Repository
+public interface VendorRepository extends MongoRepository<Vendor, String>{
+
+}
